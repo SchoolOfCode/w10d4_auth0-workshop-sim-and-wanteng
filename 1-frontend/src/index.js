1a0e22570f3dcd7a5d3c60ac430f773a3ecc7c96
Simran Toor <simrankaurtoor@outlook.com>
2022-06-30 16:20:08 +0100

    task 1: login button added

--- 1-frontend/src/index.js
+++ 1-frontend/src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const localhost = "www.google.com";
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<React.StrictMode>
+  <Auth0Provider
+    domain="dev-6x6ilyd8.us.auth0.com"
+    clientId="3hto8VkE16fLJrxEJR73asr9jLeCUSh6"
+    redirectUri={localhost}>
+    
+      <App />
+  </Auth0Provider>    
+</React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
